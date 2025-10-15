07235261a179e62655f0c49d2739c3f3c9a6c42a
debian-professional <aksme@aksmenice.de>
2025-10-15 04:51:10 -0400

    15.10.2025

---
+++ etc/config/scripts/directorys.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# directorys
+
+ln -s /home/source/debian-vpn-gateway/etc/config/backup backup
+ln -s /home/source/debian-vpn-gateway/etc/config/deb deb
+ln -s /home/source/debian-vpn-gateway/etc/config/doc doc
+ln -s /home/source/debian-vpn-gateway/etc/config/scripts scripts
+
+# scripts
+
+ln -s /home/source/debian-vpn-gateway/etc/config/firewall.sh firewall.sh
+ln -s /home/source/debian-vpn-gateway/etc/config/get-nordvpn.sh get-nordvpn.sh
+ln -s /home/source/debian-vpn-gateway/etc/config/get-pi-hole.sh get-pihole.sh
+ln -s /home/source/debian-vpn-gateway/etc/config/off.sh off.sh
+
