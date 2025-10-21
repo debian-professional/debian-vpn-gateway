ec2523d736a541162302eb2fccf73f2870fe0608
debian-professional <aksme@aksmenice.de>
2025-10-21 21:26:39 +0200

    21.10.2025

--- etc/config/scripts/ssh1.sh
+++ etc/config/scripts/ssh1.sh
@@ -11,7 +11,23 @@ p8=$(cat ~/p8)
 p9=$(cat ~//p9)
 p10=$(cat ~/p10)
 
-sshpass $p1 $p2 $p3 $p4 $p5 $p6 $p7 $p8 $p9 ${p10}
+rm ~/p1 > /dev/null 2>&1
+rm ~/p2 > /dev/null 2>&1
+rm ~/p3 > /dev/null 2>&1
+rm ~/p4 > /dev/null 2>&1
+rm ~/p5 > /dev/null 2>&1
+rm ~/p6 > /dev/null 2>&1
+rm ~/p7 > /dev/null 2>&1
+rm ~/p8 > /dev/null 2>&1
+rm ~/p9 > /dev/null 2>&1
+rm ~/p10 > /dev/null 2>&1
+
+if [ -f /etc/config/cfg/nvpn ]; then
+   ssh $p4 $p5 $p6 $p7 $p8 $p9 ${p10}
+else
+   sshpass $p1 $p2 $p3 $p4 $p5 $p6 $p7 $p8 $p9 ${p10}
+fi
+
 
 
 
