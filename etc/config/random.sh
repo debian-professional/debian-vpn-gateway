4db0a032d43ae552243e380e39df4dc3cbb778ed
debian-professional <aksme@aksmenice.de>
2025-11-09 13:48:36 +0000

    09.11.2025

--- etc/config/random.sh
+++ etc/config/random.sh
@@ -11,8 +11,13 @@ i=0
 while [ $i -ne 20 ]
 do
         i=$(($i+1))
-        curl --silent --proxy socks5h://172.29.255.1:9050  https://www.boredbutton.com/random | \
-        grep iframe | grep src |  tr '"' ' ' | awk '{print $3}' >> /tmp/links
+        if [ -f /etc/config/cfg/gateway ]; then
+           curl --silent --proxy socks5h://172.29.255.1:9050  https://www.boredbutton.com/random | \
+           grep iframe | grep src |  tr '"' ' ' | awk '{print $3}' >> /tmp/links
+        else
+           curl --silent https://www.boredbutton.com/random | \
+           grep iframe | grep src |  tr '"' ' ' | awk '{print $3}' >> /tmp/links
+        fi
 done
 echo random list generated ......
 
@@ -30,56 +35,99 @@ while read -r LINE
 
       if [ "$rvalue" -eq 1 ]; then
          echo 1 : $url
-         curl --silent --proxy socks5h://172.29.255.1:1080 $url > /dev/null 2>&1
+         if [ -f /etc/config/cfg/gateway ]; then
+            curl --silent --proxy socks5h://172.29.255.1:1080 $url > /dev/null 2>&1
+         else
+            curl --silent $url > /dev/null 2>&1
+         fi
       fi
 
       if [ "$rvalue" -eq 2 ]; then
          echo 2 : $url
-         curl --silent --proxy socks5h://172.29.255.1:1081 $url > /dev/null 2>&1
+         if [ -f /etc/config/cfg/gateway ]; then
+            curl --silent --proxy socks5h://172.29.255.1:1081 $url > /dev/null 2>&1
+         else
+            curl --silent $url > /dev/null 2>&1
+         fi
       fi
 
       if [ "$rvalue" -eq 3 ]; then
          echo 3 : $url
-         curl --silent --proxy socks5h://172.29.255.1:1082 $url > /dev/null 2>&1
+         if [ -f /etc/config/cfg/gateway ]; then
+            curl --silent --proxy socks5h://172.29.255.1:1082 $url > /dev/null 2>&1
+         else
+            curl --silent $url > /dev/null 2>&1
+         fi
       fi
 
       if [ "$rvalue" -eq 4 ]; then
          echo 4 : $url
-         curl --silent --proxy socks5h://172.29.255.1:9050 $url > /dev/null 2>&1
+         if [ -f /etc/config/cfg/gateway ]; then
+            curl --silent --proxy socks5h://172.29.255.1:9050 $url > /dev/null 2>&1
+         else
+            curl --silent $url > /dev/null 2>&1
+         fi
       fi
 
       if [ "$rvalue" -eq 5 ]; then
          echo 5 : $url
-         curl --silent --proxy socks5h://172.29.255.1:1082 $url > /dev/null 2>&1
+         if [ -f /etc/config/cfg/gateway ]; then
+            curl --silent --proxy socks5h://172.29.255.1:1082 $url > /dev/null 2>&1
+         else
+            curl --silent $url > /dev/null 2>&1
+         fi
       fi
 
       if [ "$rvalue" -eq 6 ]; then
          echo 6 : $url
-         curl --silent --proxy socks5h://172.29.255.1:9050 $url > /dev/null 2>&1
+         if [ -f /etc/config/cfg/gateway ]; then
+            curl --silent --proxy socks5h://172.29.255.1:9050 $url > /dev/null 2>&1
+         else
+            curl --silent $url > /dev/null 2>&1
+         fi
       fi
 
       if [ "$rvalue" -eq 7 ]; then
          echo 7 : $url
-         curl --silent --proxy socks5h://172.29.255.1:1082 $url > /dev/null 2>&1
+         if [ -f /etc/config/cfg/gateway ]; then
+            curl --silent --proxy socks5h://172.29.255.1:1082 $url > /dev/null 2>&1
+         else
+            curl --silent $url > /dev/null 2>&1
+         fi
       fi
 
       if [ "$rvalue" -eq 8 ]; then
          echo 8 : $url
-         curl --silent --proxy socks5h://172.29.255.1:1081 $url > /dev/null 2>&1
+         if [ -f /etc/config/cfg/gateway ]; then
+            curl --silent --proxy socks5h://172.29.255.1:1081 $url > /dev/null 2>&1
+         else
+            curl --silent $url > /dev/null 2>&1
+         fi
       fi
 
       if [ "$rvalue" -eq 9 ]; then
          echo 9 : $url
-         curl --silent --proxy socks5h://172.29.255.1:1080 $url > /dev/null 2>&1
+         if [ -f /etc/config/cfg/gateway ]; then
+            curl --silent --proxy socks5h://172.29.255.1:1080 $url > /dev/null 2>&1
+         else
+            curl --silent $url > /dev/null 2>&1
+         fi
       fi
 
       if [ "$rvalue" -eq 10 ]; then
          echo 10 : $url
-         curl --silent $url > /dev/null 2>&1
+         if [ -f /etc/config/cfg/gateway ]; then
+            curl --silent $url > /dev/null 2>&1
+         else
+            curl --silent $url > /dev/null 2>&1
+         fi
       fi
 
       line_number=$((line_number+1))
-      sleep $rvalue
+
+      if [ -f /etc/config/cfg/gateway ]; then
+         sleep $rvalue
+      fi
 
 done < "$INFILE"
 echo random list all destinations visited ....
