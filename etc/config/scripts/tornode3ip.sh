dd013efb474169b3f9b60fe85a311b3f8e695293
debian-professional <aksme@aksmenice.de>
2025-11-26 12:44:18 +0000

    26.11.2025

--- etc/config/scripts/tornode3ip.sh
+++ etc/config/scripts/tornode3ip.sh
@@ -8,7 +8,24 @@ while true
  do
    rm /etc/config/scripts/tornode3ip.log > /dev/null 2>&1
    curl --silent --proxy socks5h://172.29.255.1:9050 'https://wtfismyip.com/yaml' | grep YourFucking > /etc/config/scripts/tornode3ip.log 2>&1
-   sleep 600
+   rm /tmp/links2 > /dev/null 2>&1
+   i=0
+   while [ $i -ne 20 ]
+   do
+        i=$(($i+1))
+        curl --silent --proxy socks5h://172.29.255.1:9050  https://www.boredbut>
+        grep iframe | grep src |  tr '"' ' ' | awk '{print $3}' >> /tmp/links2
+    done
+    line_number="1"
+    INFILE=/tmp/links2
+    while read -r LINE
+    do
+      url=$(printf '%s\n' "$LINE"| awk '{print $1}')
+      rvalue=$(shuf -i 1-3 -n 1 -r)
+      curl --silent --proxy socks5h://172.29.255.1:9050 $url > /dev/null 2>&1
+      sleep $rvalue
+      line_number=$((line_number+1))
+    done < "$INFILE"
  done
 
 
