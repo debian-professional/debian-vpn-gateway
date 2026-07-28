01af878c8564d34ebb565b028be2a0cd1fe5ecd5
debian-professional <aksme@aksmenice.de>
2026-07-28 09:09:35 +0200

    27.07.2026

--- etc/config/backup/backup.sh
+++ etc/config/backup/backup.sh
@@ -64,7 +64,7 @@ if [ -e /etc/config/cfg/swtor_ssh_port1 -o -e /etc/config/cfg/swtor_ssh_port2 ] 
 fi
 
 # Wireguard
-if [ -e /etc/config/cfg/wireguard_interface1 -o -e /etc/config/cfg/wireguard_interface2 ] ; then
+if [ -e /etc/wireguard ] ; then
    tar cvzf tmp/wg.tar.gz /etc/wireguard  > /dev/null 2>&1
 fi
 
@@ -113,7 +113,7 @@ fi
 
 # Nice to have files inside of the backup
 
-tar cvzf tmp/nice_to_have.tar.gz /home/admin /home/source /etc/apt /etc/resolv.conf /etc/hosts /etc/hostname /etc/motd /etc/sudoers \
+tar cvzf tmp/nice_to_have.tar.gz /home/source/ /etc/apt /etc/resolv.conf /etc/hosts /etc/hostname /etc/motd /etc/sudoers \
 /home/source/debian-vpn-configuration/.git/config /home/source/debian-vpn-gateway/.git/config > /dev/null 2>&1
 dpkg -l > tmp/installed-software
 echo Backup created on $(date '+%Y-%m-%d-%H-%M')  > /etc/config/backup/tmp/backup-was-made.log
